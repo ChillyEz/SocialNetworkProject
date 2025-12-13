21d0c6489b96bca4b22ec593e50246e2d2edbedf
chillyez <bayseitov.2016@gmail.com>
2025-12-13 11:52:52 +0500

    testStart

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    global a[][2]= {};
+
+    public static void getquery() {
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("введи имя");
+        String name = sc.nextline();
+        System.out.println(name + ", введите деньги");
+        String money = sc.nextline();
+        setDonatation(name, money);
+    }
+
+    Private static void setDonation(String name, string money){
+        auto name1 = 1;
+
+
+    }
+
+
+
+}
