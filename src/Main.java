61a1f602405ad1c2b3c4acc53cd39b119a364475
chillyez <bayseitov.2016@gmail.com>
2025-12-18 13:36:44 +0500

    next commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main{
         SocialNetwork network = new SocialNetwork("MySocial");
         network.welcome();
 
-        Profile user1= new Profile("Alex",21git);
+        Profile user1= new Profile("Alex",20);
         Profile user2 = new Profile("Amir",20);
 
         user2.setBio("Student of AITU");
