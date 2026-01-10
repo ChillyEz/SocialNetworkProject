11d1b7b8b91f5bb7355a7555f08470bb071d73d4
chillyez <bayseitov.2016@gmail.com>
2026-01-10 10:42:15 +0500

    added example of code work into Main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,37 @@
+import java.util.ArrayList;
+
 public class Main{
     public static void main(String[] args) {
 
         SocialNetwork network = new SocialNetwork("MySocial");
         network.welcome();
+        System.out.println();
+
 
         Profile user1= new Profile("Alex",17);
         Profile user2 = new Profile("Amir",21);
+        Profile user3 = new Profile("Arman",19);
+        Profile user4 = new Profile("Zhangeldin", 22);
+        Admin admin = new Admin("SuperAdmin");
 
         user2.setBio("Student of AITU");
+        user3.setBio("Java developer");
+        admin.setBio("SuperAdmin");
+
 
         network.addUser(user1);
         network.addUser(user2);
+        network.addUser(user3);
+        network.addUser(user4);
+        network.addUser(admin);
+///
+        System.out.println("===Фильтр По возрасту===");
+        ArrayList<Profile> adults = network.filterByAge(20);
+        for(Profile p : adults) {
+            System.out.println( "Username: " + p.getUsername() + " age: " + p.getAge());
+        }
+        System.out.println();
 
-
-        ///
         network.showUser();
     }
 }
